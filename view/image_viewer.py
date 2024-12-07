41ca2d6725d10e9b71b0846117a1c3d30af2f0b8
AhmedAmgadEid <ahmedamgad16746@gmail.com>
2024-12-07 13:36:13 +0200

    add image controls ui

--- view/image_viewer.py
+++ view/image_viewer.py
@@ -1,5 +1,7 @@
-from PyQt5.QtWidgets import QWidget,QHBoxLayout,QVBoxLayout
+from PyQt5.QtWidgets import QWidget,QHBoxLayout,QVBoxLayout,QComboBox,QSlider,QLabel
 import pyqtgraph as pg
+from PyQt5.QtCore import Qt
+
 
 
 class CustomImageView(pg.ImageView):
@@ -29,6 +31,37 @@ class ImageViewer(QWidget):
         self.image_controls_widget = QWidget()
         self.image_controls_widget_layout = QVBoxLayout(self.image_controls_widget)
         self.main_widget_layout.addWidget(self.image_controls_widget)
+        ft_components_options = ["FT Magnitude", "FT Phase", "FT Real Components", "FT Imaginary Components"]
+        self.ft_components_combobox = QComboBox()
+        self.ft_components_combobox.addItems(ft_components_options)
+        self.image_controls_widget_layout.addWidget(self.ft_components_combobox)
+
+        self.brightness_control_widget = QWidget()
+        self.brightness_control_widget_layout = QHBoxLayout(self.brightness_control_widget)
+        self.contrast_control_widget = QWidget()
+        self.contrast_control_widget_layout = QHBoxLayout(self.contrast_control_widget)
+        self.image_controls_widget_layout.addWidget(self.brightness_control_widget)
+        self.image_controls_widget_layout.addWidget(self.contrast_control_widget)
+
+        self.brightness_label = QLabel("Brightness")
+        self.brightness_slider = QSlider(Qt.Horizontal)
+        self.brightness_slider.setRange(-100, 100) 
+        self.brightness_slider.setValue(0) 
+        self.brightness_control_widget_layout.addWidget(self.brightness_label)
+        self.brightness_control_widget_layout.addWidget(self.brightness_slider)
+        
+        self.contrast_label = QLabel("Contrast")
+        self.contrast_slider = QSlider(Qt.Horizontal)
+        self.contrast_slider.setRange(1, 500)  
+        self.contrast_slider.setValue(100)  
+        self.contrast_control_widget_layout.addWidget(self.contrast_label)
+        self.contrast_control_widget_layout.addWidget(self.contrast_slider)
+        
+        
+        
+
+        
+        
         
 
     
