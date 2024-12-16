2be1676592b31bf9f1ce4ef239518278d92069ce
AbdullahMahmoud2003 <116839669+AbdullahMahmoud2003@users.noreply.github.com>
2024-12-16 21:57:50 +0200

    installing the logging

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import sys
 from PyQt5.QtWidgets import QApplication
 from view.main_window import MainWindow
-
+import logging
+logger = logging.getLogger(__name__)
 
 def main():
+    logging.basicConfig(filename='myapp.log', level=logging.INFO, filemode="w", 
+                        format="%(asctime)s - %(levelname)s - %(message)s")
+    logger.info("Started")
     app = QApplication(sys.argv)
     main_window = MainWindow()
     main_window.show()
